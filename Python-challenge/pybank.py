2049c1b701b9ffb9b7e1a3473f3f32fe3bf5ab59
faithlierheimer <flierheimer@gmail.com>
2020-06-28 14:31:47 -0600

    trying to write text file

--- Python-challenge/pybank.py
+++ Python-challenge/pybank.py
@@ -11,26 +11,27 @@ budget.columns = ['date', 'pandl']
 print(budget.head())
 outfile = r'C:\Users\flier\DABootCampLierheimer\Python-challenge\pybankoutput.txt'
 #Print total number of months included in the dataset, w/labels
-print("Total Months: " + str(budget.date.count()), file = open(outfile, 'a'))
+print("Total Months: " + str(budget.date.count()))
 
 #Print net total amount of profits and losses in entire period
 #Column name for Profits/Losses abbreviated to "pandl"
-print("Net total over entire period: $" + str(budget.pandl.sum()),
-     file = open(outfile, "a"))
+print("Net total over entire period: $" + str(budget.pandl.sum()))
 
 #Print average of changes in "Profit/Losses over entire period"
-print("Average Change : $" + str(budget.pandl.mean()),
-     file = open(outfile, "a"))
+print("Average Change : $" + str(budget.pandl.mean()))
 
 #Print greatest increase in profits (date and amount) over entire period
 maxDate = budget.max()
-print("The greatest increase in profits was: ", 
-     file = open(outfile, "a"))
-print(maxDate, file = open(outfile, "a"))
+print("The greatest increase in profits was: ")
+print(maxDate)
 
 #Print greatest decrease in losses (date and amount) over the entire period
 minDate = budget.min()
-print("The greatest decrease in profits was: ", file = outfile, "r")
-print(minDate, file = outfile, "r")
+print("The greatest decrease in profits was: ")
+print(minDate)
 
 #Export all print statements to a .txt file
+with open(outfile, 'r') as text:
+     print(text)
+     lines = text.read()
+     print(lines)
