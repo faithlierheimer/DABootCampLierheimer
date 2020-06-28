642eb612612e2ee0867391c81ae4329095b9a7c8
faithlierheimer <flierheimer@gmail.com>
2020-06-28 14:25:58 -0600

    weird version of saving to .txt file

--- Python-challenge/pybank.py
+++ Python-challenge/pybank.py
@@ -9,28 +9,28 @@ print(budget.head())
 #Rename columns for ease of use
 budget.columns = ['date', 'pandl']
 print(budget.head())
-
+outfile = r'C:\Users\flier\DABootCampLierheimer\Python-challenge\pybankoutput.txt'
 #Print total number of months included in the dataset, w/labels
-print("Total Months: " + str(budget.date.count()), file = open('C:\Users\flier\DABootCampLierheimer\output.txt', 'a'))
+print("Total Months: " + str(budget.date.count()), file = open(outfile, 'a'))
 
 #Print net total amount of profits and losses in entire period
 #Column name for Profits/Losses abbreviated to "pandl"
 print("Net total over entire period: $" + str(budget.pandl.sum()),
-     file = open("C:\Users\flier\DABootCampLierheimer\output.txt", "a"))
+     file = open(outfile, "a"))
 
 #Print average of changes in "Profit/Losses over entire period"
 print("Average Change : $" + str(budget.pandl.mean()),
-     file = open("C:\Users\flier\DABootCampLierheimer\output.txt", "a"))
+     file = open(outfile, "a"))
 
 #Print greatest increase in profits (date and amount) over entire period
 maxDate = budget.max()
 print("The greatest increase in profits was: ", 
-     file = open("C:\Users\flier\DABootCampLierheimer\output.txt", "r"))
-print(maxDate, file = open("C:\Users\flier\DABootCampLierheimer\output.txt", "r"))
+     file = open(outfile, "a"))
+print(maxDate, file = open(outfile, "a"))
 
 #Print greatest decrease in losses (date and amount) over the entire period
 minDate = budget.min()
-print("The greatest decrease in profits was: ", file = "C:\Users\flier\DABootCampLierheimer\output.txt", "r")
-print(minDate, file = "C:\Users\flier\DABootCampLierheimer\output.txt", "r")
+print("The greatest decrease in profits was: ", file = outfile, "r")
+print(minDate, file = outfile, "r")
 
 #Export all print statements to a .txt file
