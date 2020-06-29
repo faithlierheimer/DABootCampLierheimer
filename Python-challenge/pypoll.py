1c6cacbcfe7c82233180898223acd3f3216dd58f
faithlierheimer <flierheimer@gmail.com>
2020-06-29 13:37:34 -0600

    first chunk of pypoll work

--- Python-challenge/pypoll.py
+++ Python-challenge/pypoll.py
@@ -1,3 +1,18 @@
 import pandas as pd 
-poll = pd.read_csv('election_data.csv')
-print(poll.head())
+
+#Import and read CSV, inspect first 5 rows. 
+poll = pd.read_csv(r'C:\Users\flier\DABootCampLierheimer\Python-challenge\election_data.csv')
+
+#print(poll.head()) [Commenting out print rows after I use them for sake of code cleanliness]
+#Change Column names for ease of use 
+poll.columns = ['voterID', 'county', 'candidate']
+
+#Count total number of votes cast, save as variable
+votesTotal = poll.voterID.count()
+
+#List of unique candidates that received votes, save as variable
+candidates = poll.candidate.unique()
+candidateList = candidates.tolist()
+#print(candidateList)
+
+#Calculate percentage of votes each candidate received, save as variable
