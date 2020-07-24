11ed69253d1842c7982cd1ef4528b1e9f6d24aa1
faithlierheimer <flierheimer@gmail.com>
2020-07-23 21:26:08 -0600

    Trying to change organization

--- python-apis-challenge/weatherPy/api_keys.py
+++ python-apis-challenge/weatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2eb6865e01332decbf5ca25a1c61cbb5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDZRntWfY_KwKSwv97wEzdS_wAQeyfwxAQ"
